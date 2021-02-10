88f014d52765c984f065e5925106a4d0f7f789a5
Edgar Herrera <edgarahg13@hotmail.com>
2021-02-10 12:55:40 -0600

    ctrlledspanish

--- firmware/card_business-ch55x/card_business-ch55x.ino
+++ firmware/card_business-ch55x/card_business-ch55x.ino
@@ -5,6 +5,10 @@
 
 #include "src/USBMassStorage.h"
 
+
+#define LED 33 //Pin 33 de la placa
+
+//Información de contacto que no cambiará
 __code const uint8_t ContactoFileContent[] = "Información de contacto\n
 Nombre: XXXXXXXXXXXXXXXXXX\n
 Teléfono: 449-XXX-XX-XX\n
@@ -12,7 +16,32 @@ Correo: XXXXXXXXXX@gmail.com\n
 Página web: electroniccats.com \n
 ";
 
+//txt para controlar Led
+__code const uint8_t Ctrl_ledFileContent[] = 
+"1 \n
+En este txt podrás encender o apagar el led de la placa, cambiando el valor del renglón de arriba y\n
+despues guardando el archivo (ctrl + g).\n
+1 - Led Prendido \n
+0 - Led Apagado \n
+";
 
+//función para el control del led
+void Ctrl_ledCallback (uint16_t offset){
+ for (uint8_t i = 0; i < BULK_MAX_PACKET_SIZE; i++) {
+  uint8_t inputVal = BOT_Rx_Buf[i];
+   if (offset == 0) {
+    if (inputVal & 1) { 
+     digitalWrite(LED, HIGH);
+    } else { 
+        digitalWrite(LED, LOW);
+    }
+   }
+   offset++;
+ }
+}
+
+
+//Creación de archivos
 __code File_Entry filesOnDrive [] = {
   {
    //.filename = {'C','O','N','T','A','C','T','O','D','O','C'}, //crea un archivo .doc
@@ -23,8 +52,18 @@ __code File_Entry filesOnDrive [] = {
    .fileCallBackType = CONST_DATA_FILE,
    .fileReadHandler = {.constPtr = ContactoFileContent},
    .fileWriteHandler = 0,
+  },
+  {
+   .filename = {'C','T','R','L','_','L','E','D','T','X','T'},
+   .filetime = {TIME_LB(12, 34, 56), TIME_HB(12, 34, 56)},
+   .filedate = {DATE_LB(2021,1,8), DATE_HB(2021, 1 ,8)},
+   .filesize = sizeof(Ctrl_ledFileContent),
+   .fileCallBackType = CONST_DATA_FILE,
+   .fileReadHandler = {.constPtr = Ctrl_ledFileContent},
+   .fileWriteHandler = Ctrl_ledCallback, 
   }
 };
+
 
 __code uint8_t filesOnDriveCount = sizeof(filesOnDrive) / sizeof(File_Entry);
 
